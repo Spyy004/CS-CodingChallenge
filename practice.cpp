1cb26a87cc7a1e3355eb7cc460ffc3b46dfa69e5
Ayush Pawar <54628130+Spyy004@users.noreply.github.com>
2021-09-30 23:32:24 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,32 +1,8 @@
-class Solution {
-public:
-    bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<pair<int,int>>pickup,drop;
-        for(int i=0;i<trips.size();i++)
-        {
-            pickup.push_back({trips[i][1],trips[i][0]});
-            drop.push_back({trips[i][2],trips[i][0]});
-        }
-        sort(pickup.begin(),pickup.end());
-        sort(drop.begin(),drop.end());
-        int currentSize=0;
-        int i=1,j=0;
-        currSize+=pickup[0].second;
-        while(i<pickup.size() && j<drop.size())
-        {
-            if(currSize>=capacity)
-                return false;
-            if(pickup[i].first<drop[j].first)
-            {
-                currSize+=pickup[i].second;
-                i++;
-            }
-            else
-            {
-                currSize-=drop[j].second;
-                j++;
-            }
-        }
-        return true;
-    }
-};
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n;
+    cin>>n;
+    cout<<((n/3)*2)+1<<endl;
+}
